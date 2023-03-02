651440d31a686339ddd8b43bb1c4a10197392064
Tahir <tahir2001@hotmail.com>
2023-03-02 23:37:43 +0300

    overall refactor

--- src/test.js
+++ src/test.js
@@ -13,75 +13,61 @@ async function scrapeHotels() {
     const startTime = new Date();
     page.goto("https://www.booking.com/hotel/gb/birds-nest-residency.en-gb.html?aid=304142&label=gen173nr-1FCAQoggI46wdICVgEaOQBiAEBmAEJuAEXyAEM2AEB6AEB-AEDiAIBqAIDuALZsv6fBsACAdICJDQ0MTliYzVjLTdkOTQtNDdjNy1hMTRjLWIxNmEzMmRjZGE0NNgCBeACAQ&sid=72a9d1104ff45429504706b924efcdd4&all_sr_blocks=889154202_358921867_2_0_0;checkin=2023-03-03;checkout=2023-03-04;dest_id=-2601889;dest_type=city;dist=0;group_adults=2;group_children=0;hapos=4;highlighted_blocks=889154202_358921867_2_0_0;hpos=4;matching_block_id=889154202_358921867_2_0_0;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=889154202_358921867_2_0_0__21600;srepoch=1677698182;srpvid=ce458782d6e902e0;type=total;ucfs=1&#hotelTmpl").catch((e) => e);
 
-    await page.waitForSelector('[data-testid="location-block-container"]');
-    // const test = await page.$eval('[data-testid="location-block-container"]', (el) => el.innerHTML)
-    //
-    // console.log(test)
 
-
-
-    // const test = await page.$eval('[data-testid="location-block-container"]', (el) => {
-    //     const element = el.querySelector('[data-testid="property-section--content"]');
-    //     return element.outerHTML
-    // });
-    //
-    // console.log(test)
-
-
-
+    // Working, gets the list of close places
     await page.waitForSelector('ul[data-location-block-list="true"]');
-    // const testArray = await page.$$eval('ul[data-location-block-list="true"]', (elements) => {
-    //     return elements.map((element) => {
-    //         const liListElement = element.innerHTML;
-    //         // liListElement.
-    //     });
-    // });
-
-
-
-    // const testArray = await page.$$eval('ul[data-location-block-list="true"]', (liLists) => {
-    //     return liLists.map(liList => {
-    //         return Array.from(liList.querySelectorAll('li'))
-    //     });
-    // })
-
-    // const testArray = await page.$$eval('ul[data-location-block-list="true"] li', (liElements) => {
-    //     return liElements.map(li => li.textContent.trim());
-    // });
-
-    // const ulList = await page.$$('ul[data-location-block-list="true"]');
-    // const testArray = await Promise.all(ulList.map(async (ul) => {
-    //     const liList = await ul.$$('li');
-    //     return liList.map((li) => li.textContent);
-    // }));
-    //
-    // console.log(testArray);
-
-    //working
-    const testArray = await page.$$eval('ul[data-location-block-list="true"]', (liLists) => {
-        return liLists.map(liList => {
-            return Array.from(liList.querySelectorAll('li')).map(li =>
-                li.innerText.replace(/([A-Z])/g, ' $1').replace('\n', ' ').trim());
+    const testArray = await page.$$eval('ul[data-location-block-list="true"]',
+        liLists => {
+            return liLists.map(liList => {
+                const title = liList.parentElement.innerText.trim()
+                    .replace(liList.innerText, "").replace("\n", "");
+                const locations = Array.from(liList.querySelectorAll('li')).map(li =>
+                    li.innerText.replace(/([A-Z])/g, ' $1')
+                        .replace('\n', ' ').trim());
+                return {title, locations}
+            });
         });
-    });
-
     console.log(testArray);
 
-    // gives titles
+    // Working gives location titles, redundant now
+    // const elements = await page.$$('div.b3d1cacd40.cc56d568f0 > div.ac78a73c96.f0d4d6a2f5.fda3b74d0d');
+    // const texts = await Promise.all(elements.map(element => element.evaluate(node => node.textContent.trim())));
+    // console.log(texts);
 
-    const elements = await page.$$('div.b3d1cacd40.cc56d568f0 > div.ac78a73c96.f0d4d6a2f5.fda3b74d0d');
-    const texts = await Promise.all(elements.map(element => element.evaluate(node => node.textContent.trim())));
-    console.log(texts);
+    // Working gives summary
+    await page.waitForSelector('#property_description_content');
+    const summary = await page.$$eval('#property_description_content > p',
+        elements => elements.map(element => element["innerText"].trim()));
+    console.log(summary)
 
 
+    // Working get Most popular facilities
+    const popularFacilities = await page.$$eval('[data-testid="facility-list-most-popular-facilities"] > div',
+        elements => {
+            const title = 'Most popular facilities';
+            const facilities = elements.map(element => element["innerText"].trim());
+            return {title, facilities};
+        });
+    console.log(popularFacilities);
 
-    // const content = await page.$('[data-testid="location-block-container"]');
-    // const ul = await content.$('ul[data-location-block-list="true"]');
-    // console.log(ul)
+    // Working get facilities and titles
+    const facilities = await page.$$eval('[data-testid="facility-group-icon"', elements => {
+        return elements.map(element => {
+            const parentElement = element.parentElement;
+            const title = parentElement.innerText;
+            const properties = parentElement.parentElement.parentElement.parentElement.innerText
+                .replace(title, "").trim().split("\n")
+            return {title, properties}
+        });
+    });
+    console.log(facilities);
 
     const endTime = new Date();
     const elapsedTime = endTime - startTime;
-    // browser.close().catch((e) => e);
+    browser.close().catch((e) => e);
+
+    console.log(testArray)
     console.log(`Elapsed time scrape hotels: ${elapsedTime}ms`);
 }
+
 scrapeHotels().then(r => r)
