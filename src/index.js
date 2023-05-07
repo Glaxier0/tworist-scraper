90606c83bcf91654641993e673453d0a40f038bf
Tahir <tahir2001@hotmail.com>
2023-05-08 00:14:12 +0300

    added browser manager

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const app = require('./app')
+const {initBrowsers, closeBrowsers} = require('./services/puppeteerBrowser')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
+});
+
+(async () => {
+    initBrowsers(3).then(() => {
+        console.log("Browsers initialized.")
+    })
+})();
+
+process.on('SIGINT', async () => {
+    await closeBrowsers();
+    process.exit(0);
 });
 
 app.get('/', (req, res) => res.json('API is Up.'));
